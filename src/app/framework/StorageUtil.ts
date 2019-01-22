9eb8f60d6228ac9a94b3a1b4cf7039171a2687f2
rajnishcoder <getwere@gmail.com>
2019-01-22 08:54:46 +0530

    storage half

--- src/app/framework/StorageUtil.ts
+++ src/app/framework/StorageUtil.ts
@@ -4,4 +4,12 @@ export class StorageUtil {
         localStorage.setItem(key, value);
     }
 
+    static getItem(key: string) {
+        return localStorage.getItem(key);
+    }
+
+    static getConvertedObject(key: string) {
+        return this.getItem(key);
+    }
+
 }
